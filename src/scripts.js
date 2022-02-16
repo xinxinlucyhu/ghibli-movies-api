2e376875637799bbfa71d011e10eb70eaed43438
xinxinlucyhu <673889807@qq.com>
2022-02-15 19:14:32 -0500

    first commit

---
+++ src/scripts.js
@@ -0,0 +1,38 @@
+const app=document.getElementById('root')
+const logo=document.createElement('img')
+logo.src='logo.png'
+const container=document.createElement('div')
+container.setAttribute('class', 'container')
+app.appendChild(logo)
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function (){
+    var data = JSON.parse(this.response)
+    if(request.status >=200 && request.status <400){
+    data.forEach(movie =>{
+
+       const card=document.createElement('div')
+       card.setAttribute('class', 'card')
+       const h1 = document.createElement('h1')
+       h1.textContent=movie.title
+       const p= document.createElement('p')
+       movie.description=movie.description.substring(0,300)
+       p.textContent=`${movie.description}...`
+       card.style.backgroundImage = `url(${movie.image})`
+      
+    
+       container.appendChild(card)
+       card.appendChild(h1)
+       card.appendChild(p)
+      
+    })
+}else{const errorMessage = document.createElement('h2')
+errorMessage.textContent=`Oh no, it's not working!`
+app.appendChild(errorMessage)
+    
+}
+}
+
+request.send()
